617fb070e9c3411533a4e76faae760cdb5829e13
Alex-huang <Alex-huang@msyc.cc>
2021-09-01 18:15:12 +0800

    js

---
+++ jishuhe.js
@@ -0,0 +1,27 @@
+// var a=0
+// var b=99
+//
+// while (b>0){
+//     a=a+b
+//     b=b-2
+// }
+//
+// console.log(a)
+//
+//
+//
+//
+// var m=new Map([["Micheal",94],["jack",98]])
+//
+// console.log(m.get("Micheal"))
+
+'use strict';
+
+var a=["A","B","C","D"]
+a.forEach(function (element,index,array){
+    console.log(element+",index:"+index+"array:"+array)
+    }
+
+
+
+);
---
+++ wow.js
@@ -0,0 +1,25 @@
+// var o = {
+//     name: 'Jack',
+//     age: 20,
+//     city: 'Beijing'
+// };
+// for (var key in o) {
+//     if (o.hasOwnProperty(key)) {
+//         console.log(key); // 'name', 'age', 'city'
+//     }
+// }
+
+var wo={
+    name:"mark",
+    age:21,
+    city: "guangzhou"
+
+
+
+};
+
+for (var i in wo){
+    if (wo.hasOwnProperty(i)){
+        console.log(i)
+    }
+}
